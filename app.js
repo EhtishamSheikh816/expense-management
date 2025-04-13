c5ea8fce8a7459d07fd6160b5eb647a1c994d5a5
Ehtisham_Sheikh <160014843+EhtishamSheikh816@users.noreply.github.com>
2025-04-13 18:40:29 +0500

    some changes

--- app.js
+++ app.js
@@ -4,6 +4,7 @@ const getRow = document.getElementById("row");
 const getPara = document.getElementById("para");
 
 function add() {
+  getPara.innerHTML = (getInpNo.value.split().reduce((acc, crr) => acc + crr));
   if (getInpTxt.value === "" || getInpNo.value === "") {
     alert("Please fill in the fields");
     return;
@@ -13,7 +14,10 @@ function add() {
     <td> ${1} </td>
     <td> ${getInpTxt.value} </td>
     <td> ${getInpNo.value} </td>
-    <td id="tdBtn" onclick="rmve(this)" > <button class="btn btn3" >Remove</button> </td>
+    <td id="tdBtn">
+    <button class="btn btn4" onclick="edt(this)">Edit</button>
+    <button class="btn btn3" onclick="rmve(this)">Remove</button>
+    </td>
   </tr>`;
   getInpTxt.value = "";
   getInpNo.value = "";
@@ -23,6 +27,11 @@ function clrAll() {
   getRow.innerHTML = "";
 }
 
+function edt(e) {
+  console.log(e.parentNode.parentNode);
+  
+}
+
 function rmve(e) {
-  console.log(e.parentNode.remove());
+  e.parentNode.remove();
 }
